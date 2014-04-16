55c3eba52bf5840e3d7c518c843968e54e4c1c14
gabrielvieira <viera_frifri@hotmail.com>
2014-04-16 18:09:50 -0300

    commit maneiro no spaco
    commit maneiro no spaco

--- tutInvader/MyScene.h
+++ tutInvader/MyScene.h
@@ -5,14 +5,21 @@
 
 //  Copyright (c) 2014 Gabriel Vieira. All rights reserved.
 //
+
+
 static const uint32_t borda     =  0x1 << 0;
 static const uint32_t tartaruga =  0x1 << 1;
 
+
 #import <SpriteKit/SpriteKit.h>
 
-@interface MyScene : SKScene
+@interface MyScene : SKScene <SKPhysicsContactDelegate>
 
 @property SKSpriteNode *spriteTut;
+@property SKSpriteNode *chao;
+@property SKSpriteNode *bg;
 @property BOOL pode;
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
 
 @end
--- tutInvader/MyScene.m
+++ tutInvader/MyScene.m
@@ -17,20 +17,42 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
+        self.bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+        self.bg.position = CGPointMake(0, size.width /2);
+        [self addChild:self.bg];
+       
         self.pode = NO;
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        
+        
+        self.chao = [[SKSpriteNode alloc]initWithColor:[UIColor yellowColor] size:CGSizeMake(2 * size.height, 1)];
+        self.chao.position = CGPointMake(0, -1);
+        self.chao.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.chao.size];
+        self.chao.physicsBody.categoryBitMask = borda;
+        self.chao.physicsBody.dynamic = NO;
+        [self addChild:self.chao];
+        
+        
         
         self.spriteTut = [SKSpriteNode spriteNodeWithImageNamed:@"tut"];
         self.spriteTut.size = CGSizeMake(40, 60);
-        self.spriteTut.position = CGPointMake(100, 300);
+        self.spriteTut.position = CGPointMake(50, 160);
         SKAction *action = [SKAction rotateByAngle:(M_PI/2) * (-1) duration:1];
         [self.spriteTut runAction:action];
-        
-        [self addChild:self.spriteTut];
         self.spriteTut.physicsBody.dynamic = YES;
         self.spriteTut.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:self.spriteTut.size.width * 0.5];
+        self.spriteTut.physicsBody.categoryBitMask = tartaruga;
+        self.spriteTut.physicsBody.contactTestBitMask = tartaruga | borda ;
+        self.spriteTut.physicsBody.collisionBitMask = tartaruga | borda ;
+       
+        [self addChild:self.spriteTut];
         
-        self.physicsWorld.gravity = CGVectorMake(0.0f, -1.0f);
+        
+        
+        self.physicsWorld.gravity = CGVectorMake(0.0f, 0.0f);
+        self.physicsWorld.contactDelegate = self;
+        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        
+        
 
     }
     return self;
@@ -60,7 +82,7 @@
 
 - (void)didBeginContact:(SKPhysicsContact *)contact
 {
-
+    NSLog(@"colisao");
 }
 
 #pragma mark Main Game Loop
@@ -68,10 +90,31 @@
 -(void)update:(CFTimeInterval)currentTime {
     
     if (self.pode) {
-         self.spriteTut.position = CGPointMake(self.spriteTut.position.x +2, self.spriteTut.position.y);
+        
+        [self atirar];
+
+        self.pode = NO;
     }
-  
+    
+    self.bg.position = CGPointMake(self.bg.position.x -1, self.bg.position.y)   ;
+    
+    if (self.bg.position.x <= -490) {
+        self.bg.position = CGPointMake(0, self.bg.position.y)   ;
+    }
+    
+   
     /* Called before each frame is rendered */
+}
+
+-(void)atirar
+{
+    SKSpriteNode *tiro = [[SKSpriteNode alloc]initWithColor:[UIColor yellowColor] size:CGSizeMake(2, 2)];
+    tiro.position = self.spriteTut.position;
+    tiro.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:tiro.size];
+    
+    tiro.physicsBody.dynamic = YES;
+    [self addChild:tiro];
+    [tiro.physicsBody applyForce:CGVectorMake(5.0f, 0.0f)];
 }
 
 @end
